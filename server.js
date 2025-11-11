cc634760116bb947c9f4bf8ba5621a055f3675ad
Bilal <your_email@example.com>
2025-11-11 00:30:31 -0800

    vercel error resolve

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 import express from "express";
+import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
-import connectDB from "./config/db.js"
 import router from "./routes/userRoutes.js";
 
 dotenv.config();
 
 const app = express();
 
+// Middleware
 app.use(express.json());
 app.use(cors());
 
-// Connect DB
-await connectDB();
+// MongoDB Connection
+connectDB();
 
+
+// Routes
 app.use("/", router);
 
-// Vercel ke liye port listen nahi hota
-export default app;
+// Test route
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
+
+// Port Setup
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
